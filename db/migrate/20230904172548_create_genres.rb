23d25c41f6283250a2df8b72979e557a9c47e0e6
Asrat77 <112464138+Asrat77@users.noreply.github.com>
2023-09-05 05:53:33 +0300

    Update 20230904172548_create_genres.rb

--- db/migrate/20230904172548_create_genres.rb
+++ db/migrate/20230904172548_create_genres.rb
@@ -1,8 +1,8 @@
 class CreateGenres < ActiveRecord::Migration[7.0]
   def change
     create_table :genres do |t|
-      t.integer :genre_id
-      t.string :name
+      t.integer :genre_id, null: false, unique: true
+      t.string :name, null: false, unique: true
 
       t.timestamps
     end
