08f561081a25054d93f0f84147c448a0ec3824e4
Asrat77 <112464138+Asrat77@users.noreply.github.com>
2023-10-03 10:50:35 +0300

    AddUserTypeToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_200225) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_074906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -106,6 +106,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_200225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_picture"
+    t.string "user_type"
   end
 
   create_table "wishlist_items", force: :cascade do |t|
