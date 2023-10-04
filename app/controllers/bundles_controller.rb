ae633df77e0f6a9955e6674ee7a1842e30db0ea1
Tsiige77 <tsigeweyni96@gmail.com>
2023-10-04 20:31:25 +0300

    created controller bundle

---
+++ app/controllers/bundles_controller.rb
@@ -0,0 +1,7 @@
+class BundlesController < ApplicationController
+    inlude Common
+
+    private 
+    def model_params
+        params.require(:payload).permit(:bundle_id, :bundle_name,:bundle_price, :bundle_discount)
+end
