854fb68b57f4a654c35568de3c4e34c7973124e0
alazarlemma02 <alazar03lemma@gmail.com>
2023-10-04 19:15:11 +0300

    created the user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  skip_before_action :authorize, only: :create
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.user_id
+      render json: @user, status: :created
+    else
+      puts @user.errors.full_messages
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+def show
+  @user = User.find_by(email: params[:email])
+  render json: @user
+end
+
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :username, :password, :password_confirmation, :user_type)
+  end
+end
