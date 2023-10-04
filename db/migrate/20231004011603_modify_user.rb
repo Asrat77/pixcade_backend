155193ddc4ee469dc29754fcc86b3d5d9cdb4fa5
alazarlemma02 <alazar03lemma@gmail.com>
2023-10-04 19:16:35 +0300

    added password, username, and user_type to the user model

--- db/migrate/20231004011603_modify_user.rb
+++ db/migrate/20231004011603_modify_user.rb
@@ -1,8 +1,6 @@
 class ModifyUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users,:external_link, :string
-    add_column :users,:profile_text, :string
-
-
+    add_column :users, :external_link, :string
+    add_column :users, :profile_text, :string
   end
 end
