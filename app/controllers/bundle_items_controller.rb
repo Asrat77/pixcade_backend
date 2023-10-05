f10164c09c14057281be60ae15dd59c2f4ffb873
Asrat77 <112464138+Asrat77@users.noreply.github.com>
2023-10-05 10:17:50 +0300

    Fix n+1 queries

--- app/controllers/bundle_items_controller.rb
+++ app/controllers/bundle_items_controller.rb
@@ -1,6 +1,12 @@
 class BundleItemsController < ApplicationController
     include Common
 
+    def index
+        super do
+            BundleItem.includes(:bundle, :game)
+        end
+    end
+
     private
     def model_params
         params.require(:payload).permit(:bundleItemID, :bundle, :game)
--- app/controllers/featureds_controller.rb
+++ app/controllers/featureds_controller.rb
@@ -1,6 +1,13 @@
 class FeaturedsController < ApplicationController
   include Common
 
+
+  def index
+    super do
+      Featured.includes(:game)
+    end
+  end
+
     private
     def model_params
         params.require(:payload).permit(:feature_id, :valid_from,:valid_until, :game_id)
