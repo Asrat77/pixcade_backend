77f8ed0fb09058497b7a25eac06722f2832c2c30
Asrat77 <112464138+Asrat77@users.noreply.github.com>
2023-10-05 11:51:47 +0300

    delegate name/release_date to game

--- app/models/bundle_item.rb
+++ app/models/bundle_item.rb
@@ -2,7 +2,7 @@ class BundleItem < ApplicationRecord
   belongs_to :bundle
   belongs_to :game
 
-
+  delegate :name, :release_date, to: :game, prefix: :game, allow_nil: true
 
   validates :bundleItemID, uniqueness: true, presence: true
   validates :bundle, uniqueness: true, presence: true
--- app/serializers/bundle_item_serializer.rb
+++ app/serializers/bundle_item_serializer.rb
@@ -1,3 +1,3 @@
 class BundleItemSerializer < ActiveModel::Serializer
-  attributes :id, :bundle, :game
+  attributes :id, :bundle, :game_name, :game_release_date
 end
